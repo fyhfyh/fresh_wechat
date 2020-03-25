53e789b87eb2d436fd7bcbb3e362a64e83e256bd
fyh <1064867470@qq.com>
2020-03-25 20:52:09 +0800

    fdfda

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ import util from './utils/util.js';
 
 App({
 
-  checkLogin_status() {
-    let token = wx.getStorageSync(CACHE_TOKEN);
-    let expiresTime = wx.getStorageSync(CACHE_EXPIRES_TIME);
-    let userInfo = wx.getStorageSync(CACHE_USERINFO); 
-    if (!userInfo) {
-      wx.redirectTo({
-        url: '/pages/user_login/index',
-      })
-    }
-    else{
-      this.globalData.token = token;
-      this.globalData.expiresTime = expiresTime;
-      this.globalData.userInfo = userInfo ? JSON.parse(userInfo) : {};
-    }
-  },
+  // checkLogin_status() {
+  //   let token = wx.getStorageSync(CACHE_TOKEN);
+  //   let expiresTime = wx.getStorageSync(CACHE_EXPIRES_TIME);
+  //   let userInfo = wx.getStorageSync(CACHE_USERINFO); 
+  //   if (!userInfo) {
+  //     wx.redirectTo({
+  //       url: '/pages/user_login/index',
+  //     })
+  //   }
+  //   else{
+  //     this.globalData.token = token;
+  //     this.globalData.expiresTime = expiresTime;
+  //     this.globalData.userInfo = userInfo ? JSON.parse(userInfo) : {};
+  //   }
+  // },
   onLaunch: function (option) {
     if (HTTP_REQUEST_URL==''){
       console.error("请配置根目录下的config.js文件中的 'HTTP_REQUEST_URL'\n\n请修改开发者工具中【详情】->【AppID】改为自己的Appid\n\n请前往后台【小程序】->【小程序配置】填写自己的 appId and AppSecret");
@@ -144,8 +144,13 @@ App({
 
   checkLogin_status(){
     let newTime = Math.round(new Date() / 1000);
-    
-    if (!wx.getStorageSync(CACHE_USERINFO) && wx.getStorageSync(CACHE_EXPIRES_TIME) < newTime ) {
+    console.log(wx.getStorageSync(CACHE_EXPIRES_TIME))
+    if (!wx.getStorageSync(CACHE_USERINFO) || wx.getStorageSync(CACHE_EXPIRES_TIME) < newTime ) {
+     wx.showToast({
+        title: '登录过期！',
+       icon: 'none',
+        duration: 3000
+      })
       wx.redirectTo({
         url: '/pages/user_login/index',
       })
